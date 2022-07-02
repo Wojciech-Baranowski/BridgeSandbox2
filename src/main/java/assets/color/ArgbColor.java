b2f026d47bdafb5ed91e47bd14de3022b4aadd54
simmondobber <wojciechfbaranowski@gmail.com>
2022-07-02 20:20:53 +0200

    ArgbColor

--- src/main/java/assets/color/ArgbColor.java
+++ src/main/java/assets/color/ArgbColor.java
@@ -5,22 +5,26 @@ public class ArgbColor implements Color {
     int value;
 
     ArgbColor(int value) {
-
+        this.value = value;
     }
 
     @Override
     public int getValue() {
-        return 0;
+        return value;
     }
 
     @Override
     public Color blend(Color color) {
-        return null;
+        float alpha = ((color.getValue() & 0xFF000000) >>> 24) * (1.0f / 255.0f);
+        int red = (int)((((color.getValue() & 0x00FF0000) >>> 16) * alpha) + (((value & 0x00FF0000) >>> 16) * (1.0f - alpha)));
+        int green = (int)((((color.getValue() & 0x0000FF00) >>> 8) * alpha) + (((value & 0x0000FF00) >>> 8) * (1.0f - alpha)));
+        int blue = (int)(((color.getValue() & 0x000000FF) * alpha) + ((value & 0x000000FF) * (1.0f - alpha)));
+        return new ArgbColor(0xFF << 24 | red << 16 | green << 8 | blue);
     }
 
     @Override
     public boolean isTransparent() {
-        return false;
+        return (value >>> 24) == 255;
     }
 
 }
--- src/test/java/assets/color/ArgbColorTest.java
+++ src/test/java/assets/color/ArgbColorTest.java
@@ -26,7 +26,7 @@ public class ArgbColorTest {
         //given
         int[] input = {0xFFFFFF00, 0xFE213769, 0xFFFFFFFF, 0x00000000, 0x12345678, 0xFFFF1234, 0x01010203};
         int[] input2 = {0x34656665, 0xFFFF00FF, 0x22334455, 0x12345678, 0x00FFFFFF, 0xFE050505, 0xEDFEAECB};
-        int[] outputCheckInt = {0xFFDFDF14, 0xFFFF00FF, 0xFFE3E6E8, 0xFF030608, 0x12345678, 0xFF050505, 0xFFECA1BC};
+        int[] outputCheckInt = {0xFFDFDF14, 0xFFFF00FF, 0xFFE3E6E8, 0xFF030608, 0xFF345678, 0xFF050505, 0xFFECA1BC};
         Color[] inputColor = new ArgbColor[input.length];
         Color[] inputColor2 = new ArgbColor[input.length];
         ArgbColor[] outputCheck = new ArgbColor[input.length];

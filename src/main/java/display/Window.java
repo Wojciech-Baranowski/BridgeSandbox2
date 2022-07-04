b3ea8c6c6e471000227105ea71003459d19eb761
simmondobber <wojciechfbaranowski@gmail.com>
2022-07-04 17:31:57 +0200

    Window tests

---
+++ src/main/java/display/Window.java
@@ -0,0 +1,30 @@
+package display;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.util.EventListener;
+
+public class Window {
+
+    private BufferStrategy bufferStrategy;
+    private JFrame jFrame;
+    private Canvas canvas;
+    private Graphics2D graphics2D;
+    private static final int x = 0;
+    private static final int y = 0;
+    static final int w = 800;
+    static final int h = 640;
+
+    Window(){
+
+    }
+
+    void draw(int[] pixels) {
+
+    }
+
+    void addListener(EventListener eventListener) {
+
+    }
+}

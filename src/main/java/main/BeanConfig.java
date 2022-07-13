83c21ab73375528cb0170e5a391b07a7bb688f10
simmondobber <wojciechfbaranowski@gmail.com>
2022-07-13 22:02:56 +0200

    BeanConfig

---
+++ src/main/java/main/BeanConfig.java
@@ -0,0 +1,19 @@
+package main;
+
+import assets.AssetsBean;
+import display.DisplayBean;
+import input.InputBean;
+import scene.SceneBean;
+
+public class BeanConfig {
+
+    BeanConfig() {}
+
+    void buildBeans() {
+        AssetsBean.getAssets();
+        DisplayBean.getDisplay();
+        InputBean.getInput();
+        SceneBean.getScene();
+    }
+
+}

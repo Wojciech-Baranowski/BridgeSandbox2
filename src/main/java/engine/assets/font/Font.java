dfb6db0f63c2725a11429e35c0341b220bd6c2ed
simmondobber <wojtek55501000@wp.pl>
2022-07-21 20:37:52 +0200

    fix

--- src/main/java/engine/assets/font/Font.java
+++ src/main/java/engine/assets/font/Font.java
@@ -7,7 +7,7 @@ public interface Font {
     Rasterable getSymbolRasterable(char symbol);
 
     static String getExtendedAlphabet() {
-        return "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\u2664\u2665\u2666\u2667\u2468\u2469";
+        return "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\u2664\u2665\u2666\u2667\u2668\u2669";
     }
 
 }

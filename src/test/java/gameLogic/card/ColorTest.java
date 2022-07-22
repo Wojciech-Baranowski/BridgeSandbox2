ce42ca7688786879a3b06648e3f29ad74361f8fe
simmondobber <wojtek55501000@wp.pl>
2022-07-22 22:40:51 +0200

    fix

--- src/test/java/gameLogic/card/ColorTest.java
+++ src/test/java/gameLogic/card/ColorTest.java
@@ -11,14 +11,14 @@ public class ColorTest {
     public void get_ascii_test() {
         //given
         Color[] inputColor = new Color[]{CLUB, DIAMOND, HEART, SPADE, NOATU, SPADE, SPADE};
-        char[] outputCheck = new char[]{'\u2667', '\u2666', '\u2665', '\u2664', '\u2668', '\u2664', '\u2664'};
+        char[] outputCheck = new char[]{'\u2667', '\u2666', '\u2665', '\u2664', '\u2668', '\u2664', '\u2664' };
         char[] output = new char[inputColor.length];
         //when
-        for(int i = 0; i < inputColor.length; i++) {
+        for (int i = 0; i < inputColor.length; i++) {
             output[i] = inputColor[i].getAscii();
         }
         //then
-        for(int i = 0; i < inputColor.length; i++) {
+        for (int i = 0; i < inputColor.length; i++) {
             assertEquals(outputCheck[i], output[i]);
         }
     }

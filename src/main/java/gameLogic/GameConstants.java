2a7a1501f38647500c6e68a5b61909406a12eab2
simmondobber <wojtek55501000@wp.pl>
2022-07-23 01:42:19 +0200

    GameConstants

---
+++ src/main/java/gameLogic/GameConstants.java
@@ -0,0 +1,10 @@
+package gameLogic;
+
+public class GameConstants {
+
+    public static int FIGURE_NUMBER = 13;
+    public static int COLOR_NUMBER = 4;
+    public static int DECK_SIZE = FIGURE_NUMBER * COLOR_NUMBER;
+    public static int PLAYER_NUMBER = 4;
+
+}
